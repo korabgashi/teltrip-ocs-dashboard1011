70258a06b205b4873dbcbb8dacfcf75f71bb259d
Korab Gashi <korabg30@gmail.com>
2025-08-17 15:26:32 +0200

    Delete app/api/echo directory

--- app/api/echo/route.js
+++
@@ -1,7 +0,0 @@
-// app/api/echo/route.js
-export async function GET() {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { 'Content-Type': 'application/json' },
-    status: 200,
-  });
-}
